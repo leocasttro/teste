c43877900e5f3b3ad0fd3fcf71444a0358bed155
leocasttro <leocasttro01@icloud.com>
2023-04-02 19:00:45 -0300

    crud complete

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -12,7 +12,7 @@ public class ApiService
         _httpClient = httpClient;
     }
 
-    public async Task<List<Data>> GetDataAsync()
+    public async Task<List<Data>?> GetDataAsync()
     {
         try
         {
@@ -26,11 +26,24 @@ public class ApiService
         }
     }
 
-    public async Task<HttpResponseMessage> CreateDataAsync(Data data)
+    public async Task<Data> GetDataByIdAsync(string id)
     {
-        var apiUrl = "https://nocodebackend.azurewebsites.net/api/v1/dataset/6426cf2fda79fc544c8e1409/611ed902fd5915f2ae005dbb?apiKey=6426ce03da79fc544c8e1406";
+        var response = await _httpClient.GetAsync($"https://nocodebackend.azurewebsites.net/api/v1/dataset/6428c0b146374be4567806ce/611ed902fd5915f2ae005dbb/{id}?apiKey=6426ce03da79fc544c8e1406 ");
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<Data>();
+        }
+        else
+        {
+            throw new Exception($"Failed to get data with id {id}: {response.ReasonPhrase}");
+        }
+    }
+
+    public async Task<HttpResponseMessage> CreateDataAsync(DataPost dataPost)
+    {
+        var apiUrl = "https://nocodebackend.azurewebsites.net/api/v1/dataset/6428c0b146374be4567806ce/611ed902fd5915f2ae005dbb?apiKey=6426ce03da79fc544c8e1406";
         
-        var json = JsonConvert.SerializeObject(data);
+        var json = JsonConvert.SerializeObject(dataPost);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
 
         var response = await _httpClient.PostAsync(apiUrl, content);
@@ -48,4 +61,16 @@ public class ApiService
 
         return response;
     }
+
+    public async Task<HttpResponseMessage> UpdateDataAsync(string id, DataUpdate dataUpdate)
+    {
+        var endpoint = $"https://nocodebackend.azurewebsites.net/api/v1/dataset/6428c0b146374be4567806ce/611ed902fd5915f2ae005dbb/{id}?apiKey=6426ce03da79fc544c8e1406";
+
+        var payload = new StringContent(JsonConvert.SerializeObject(dataUpdate),  Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PutAsync(endpoint, payload);
+
+        return response;
+    }
+
 }
--- models/ApiModel.cs
+++ models/ApiModel.cs
@@ -1,71 +1,60 @@
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
-    public class RootObject
-    {
-        public List<Data> Data { get; set; }
-        public int page { get; set; }
-        public int pageSize { get; set; }
-        public int total { get; set; }
-    }
 
-    public class Data
-    {
-        [JsonPropertyName("_id")]
-        public Id Id { get; set; }
+public class RootObject
+{
+    public List<Data>? Data { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int Total { get; set; }
+}
 
-        [JsonPropertyName("DateCreated")]
-        public DateCreated DateCreated { get; set; }
+public class Data
+{
+    [JsonPropertyName("_id")]
+    public Id Id { get; set; } 
+    public string? Nome { get; set; }
+    public string? Site { get; set; }
+    public bool Active { get; set; }
+}
 
-        [JsonPropertyName("DateModified")]
-        public DateModified DateModified { get; set; }
+public class Id
+{
+    [JsonPropertyName("$oid")]
+    [DataMember(Name = "$oid")]
+    public string Oid { get; set; }
+}
 
-        [JsonPropertyName("Active")]
-        public bool Active { get; set; }
 
-        [JsonPropertyName("CreatedByUser")]
-        public string CreatedByUser { get; set; }
 
-        [JsonPropertyName("ModifiedByUser")]
-        public string ModifiedByUser { get; set; }
+public class DataPost 
+{
+    public string? Nome { get; set; }
+    public string? Site { get; set; }
+    public string? CreatedByUser { get; set; }
+}
 
-        [JsonPropertyName("Nome")]
-        public string Nome { get; set; }
+[DataContract]
+public class DataUpdate
+{
+    [DataMember(Name = "_id")]
+    public IdUpdate Id { get; set; } 
 
-        [JsonPropertyName("Site")]
-        public string Site { get; set; }
+    [DataMember]
+    public string? Nome { get; set; }
 
-        [JsonPropertyName("Segmento")]
-        public Segmento Segmento { get; set; }
+    [DataMember]
+    public string? Site { get; set; }
 
-        [JsonPropertyName("611edbfdfd5915f2ae005dc5")]
-        public List<Segmento> Segmentos { get; set; }
-    }
+    [DataMember]
+    public bool Active { get; set; }
+}
 
-    public class Id
-    {
-        [JsonPropertyName("$oid")]
-        public string Oid { get; set; }
-    }
 
-    public class DateCreated
-    {
-        [JsonPropertyName("$date")]
-        public DateTime Date { get; set; }
-    }
+[DataContract]
+public class IdUpdate
+{
 
-    public class DateModified
-    {
-        [JsonPropertyName("$date")]
-        public DateTime Date { get; set; }
-    }
-
-    public class Segmento
-    {
-        [JsonPropertyName("$oid")]
-        public string Oid { get; set; }
-
-        [JsonPropertyName("Nome")]
-        public string Nome { get; set; }
-
-        [JsonPropertyName("Descrição")]
-        public string Descricao { get; set; }
-    }
+    [DataMember(Name = "$oid")]
+    public string Oid { get; set; }
+}
